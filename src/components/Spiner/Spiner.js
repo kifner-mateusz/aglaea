cae41d517df223d1b640a8419c5f6d4eb1fc40ec
Mateusz Kifner <kifner-mateusz@users.noreply.github.com>
2019-11-05 15:56:30 +0100

    remove not server

--- src/components/Spiner/Spiner.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Spiner.css";
-
-const Spiner = () => {
-  return <div className="loader"></div>;
-};
-
-export default Spiner;
--- src/helper/object.js
+++
@@ -1,5 +0,0 @@
-function isEmpty(obj) {
-  return Object.entries(obj).length === 0 && obj.constructor === Object;
-}
-
-export { isEmpty };
